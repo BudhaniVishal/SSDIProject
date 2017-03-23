4d9e013b640372b846d6a6810c18fcc67d1bbbad
mayuri0192 <mvd.mayuri@gmail.com>
2017-03-23 15:12:14 -0400

    Added Edistor.cshtml, Editor.js, js libraries and Editor Controller

---
+++ SSDI_SPILELApplication/SSDI_SPILELApplication/Scripts/app/Editor.js
@@ -0,0 +1,23 @@
+﻿var module = angular.module("myapp", []);
+
+ var myController2 = module.controller("EditorController", function ($scope, $http, $window) {
+     $scope.mydata = {};
+     $scope.mydata.doClick = function () {
+         var myobj = $scope.mydata;
+         //$http.post('/Home/AjaxMethod', myobj).then(function successCallback(data, status) {
+         //    alert("Successful");
+         //});
+             var post = $http({
+                 method: "POST",
+                 url: "/Home/AjaxMethod",
+                 dataType: 'json/text',
+                 data: myobj,
+                 headers: { "Content-Type": "application/json" }
+             });
+             post.then(function successCallback(response) {
+                 $window.alert("successful");
+             }),function errorCallback(response) {
+                 $window.alert("error");
+             };   
+     }
+ });
