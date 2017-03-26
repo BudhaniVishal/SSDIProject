cd8ef1b3f84199f66efc2f8a6b7669b0b0eebd3d
mayuri0192 <mvd.mayuri@gmail.com>
2017-03-26 16:54:22 -0400

    added validations

--- SSDI_SPILELApplication/SSDI_SPILELApplication/Scripts/app/Editor.js
+++ SSDI_SPILELApplication/SSDI_SPILELApplication/Scripts/app/Editor.js
@@ -1,26 +1,25 @@
 ﻿var module = angular.module("myapp", []);
 
- var myController2 = module.controller("EditorController", function ($scope, $http, $window) {
-     $scope.mydata = null;
-     $scope.myfunc = {};
-     $scope.myfunc.doClick = function () {
-         var myobj = $scope.mydata;
-         if (myobj != null) {
-             //$http.post('/Home/AjaxMethod', myobj).then(function successCallback(data, status) {
-             //    alert("Successful");
-             //});
-             var post = $http({
-                 method: "POST",
-                 url: "/Home/CreateEditorStory",
-                 dataType: 'json/text',
-                 data: myobj,
-                 headers: { "Content-Type": "application/json" }
-             });
-             post.then(function successCallback(response) {
-                 $window.alert("successful");
-             }), function errorCallback(response) {
-                 $window.alert("error");
-             };
-         }
-     }
- });
+var myController2 = module.controller("EditorController", function ($scope, $http, $window) {
+    $scope.mydata = null;
+    $scope.myfunc = {};
+    $scope.submitForm = function (isValid) {
+        // check to make sure the form is completely valid
+        if (isValid) {
+            var myobj = $scope.mydata;
+            var post = $http({
+                method: "POST",
+                url: "/Home/CreateEditorStory",
+                dataType: 'json/text',
+                data: myobj,
+                headers: { "Content-Type": "application/json" }
+            });
+            post.then(function successCallback(response) {
+                $window.alert("Story Created!");
+            }), function errorCallback(response) {
+                $window.alert("error");
+            };
+        }
+    }
+});
+
