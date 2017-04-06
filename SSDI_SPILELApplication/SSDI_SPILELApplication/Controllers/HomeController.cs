5189f879669779be0b8f2d8b2b0d2d0d744c6b4f
Vishal Budhani <vbudhani@uncc.edu>
2017-04-06 16:53:31 -0400

    Browse Stories User Story Initial Checkin

--- SSDI_SPILELApplication/SSDI_SPILELApplication/Controllers/HomeController.cs
+++ SSDI_SPILELApplication/SSDI_SPILELApplication/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace SSDI_SPILELApplication.Controllers
 {
     public class HomeController : Controller
     {
+        static List<StoryModel> storiesAvailable;
         public ActionResult Index()
         {
             AccountController.ShowLogOff = false;
@@ -37,6 +38,151 @@ namespace SSDI_SPILELApplication.Controllers
 
             return View();
         }
+
+        
+        public ActionResult BrowseStories()
+        {
+            BrowseStoryModel model = new BrowseStoryModel();
+            
+            storiesAvailable = new List<StoryModel>();
+            
+            //string strDDLValue = Request.Form["genre"].ToString();
+            for (int i = 1; i<= 10; i++){
+                StoryModel story = new StoryModel();
+                story.Title = "test Story" + i;
+                story.Content = "test Story" + i;
+                //story.Genres = genre;
+                //story.Types = type;
+                storiesAvailable.Add(story);
+            }
+            
+            //ViewBag.Test = genre;
+            ViewBag.GenreValue = "Select";
+            ViewBag.TypeValue = "Type";
+
+
+            model.GenreValues = GetGenres();
+            model.TypeValues = GetTypes();
+
+            model.Stories = storiesAvailable;
+            return View(model);
+            //return View(storiesAvailable);
+        }
+
+        public ActionResult TestView()
+        {
+            BrowseStoryModel model = new BrowseStoryModel();
+
+            storiesAvailable = new List<StoryModel>();
+
+            //string strDDLValue = Request.Form["genre"].ToString();
+            for (int i = 1; i <= 10; i++)
+            {
+                StoryModel story = new StoryModel();
+                story.Title = "test Story" + i;
+                story.Content = "test Story" + i;
+                //story.Genres = genre;
+                //story.Types = type;
+                storiesAvailable.Add(story);
+            }
+
+            //ViewBag.Test = genre;
+            ViewBag.GenreValue = "Select";
+            ViewBag.TypeValue = "Type";
+
+
+            model.GenreValues = GetGenres();
+            model.TypeValues = GetTypes();
+
+            model.Stories = storiesAvailable;
+            return View(model);
+            //return View(storiesAvailable);
+        }
+
+        private List<SelectListItem> GetTypes()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem
+            {
+                Text = "Type1",
+                Value = "Type1"
+            });
+            items.Add(new SelectListItem
+            {
+                Text = "Type2",
+                Value = "Type2",
+                Selected = true
+            });
+            items.Add(new SelectListItem
+            {
+                Text = "Type3",
+                Value = "Type3"
+            });
+            return items;
+        }
+
+        private List<SelectListItem> GetGenres()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem
+            {
+                Text = "Swimming",
+                Value = "Swimming"
+            });
+            items.Add(new SelectListItem
+            {
+                Text = "Cycling",
+                Value = "Cycling",
+                Selected = true
+            });
+            items.Add(new SelectListItem
+            {
+                Text = "Running",
+                Value = "Running"
+            });
+            return items;
+        }
+
+        
+        public ActionResult FilterStories(string SelectedGenre, string SelectedType)
+        {
+            BrowseStoryModel model = new BrowseStoryModel();
+
+            var t1 = Request.Form["GenreVal"];
+            var t = ViewBag.GenreValue;
+            var k = ViewBag.TypeValue;
+            storiesAvailable = new List<StoryModel>();
+
+            //string strDDLValue = Request.Form["genre"].ToString();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                StoryModel story = new StoryModel();
+                story.Title = "A Story" + i;
+                story.Content = "A Story" + i;
+                storiesAvailable.Add(story);
+            }
+            model.Stories = storiesAvailable;
+            model.GenreValues = GetGenres();
+            model.SelectedGenre = GetGenres().Any(x => x.Selected).ToString();
+            model.TypeValues = GetTypes();
+
+
+            ViewBag.GenreValue = "Family";
+            ViewBag.TypeValue = "Type";
+            //return View("DisplayAvailableStories", storiesAvailable);
+            return View("DisplayAvailableStories", model);
+            //http://stackoverflow.com/questions/21170064/how-to-refresh-only-part-of-the-index-page-in-mvc-5
+            //http://stackoverflow.com/questions/23851323/how-to-access-a-view-element-from-controller
+            //http://www.binaryintellect.net/articles/4a00a9ce-73e5-4d89-aaae-2d835eca0854.aspx
+
+        }
+
+        protected void ddlselect_Changed(object sender, EventArgs e)
+        {
+
+        }
+
         [HttpPost]
         public JsonResult CreateEditorStory(StoryModel data)
         {
---
+++ SSDI_SPILELApplication/SSDI_SPILELApplication/Models/BrowseStoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSDI_SPILELApplication.Models
+{
+    public class BrowseStoryModel
+    {
+        public List<StoryModel> Stories;
+        public List<SelectListItem> GenreValues;
+        public List<SelectListItem> TypeValues;
+        public string SelectedGenre;
+    }
+}
