95c010e9ab063cf41f4e48214bd3ee2ecc7a7939
Vishal Budhani <vbudhani@uncc.edu>
2017-04-08 15:56:27 -0400

    Commit

---
+++ SSDI_SPILELApplication/SSDI_SPILELApplication/Scripts/app/BrowseStory.js
@@ -0,0 +1,19 @@
+﻿var module = angular.module("BrowseStory", []);
+
+var myController2 = module.controller("BrowseStoryController", function ($scope, $http, $window) {
+    $scope.mydata = null;
+    $scope.myfunc = {};
+    $scope.myfunc.doClick = function (){
+        // check to make sure the form is completely valid
+        var myobj = $scope.mydata;
+        var post = $http({
+            method: "POST",
+            url: "/Home/FilterStories",
+            dataType: 'json/text',
+            data: myobj,
+            headers: { "Content-Type": "application/json" }
+        });
+        
+    }
+});
+
